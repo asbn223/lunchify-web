48bf573c22bfcd674ca217b44364a31ad105d111
asbn223 <asbn2231@gmail.com>
2025-05-06 13:43:12 +0545

    Fix Notification Issues

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4101669376"
+    serviceWorkerVersion: "3235359716"
   }
 });
