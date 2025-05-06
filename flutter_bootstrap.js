bf63084b6e7afff438c8ce593adf4b9d546ce71b
asbn223 <asbn2231@gmail.com>
2025-05-06 11:18:13 +0545

    Add FCM Token

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2019059881"
+    serviceWorkerVersion: "397336074"
   }
 });
